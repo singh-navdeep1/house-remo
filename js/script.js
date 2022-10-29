ed177a9bcd43b504665d741c2dc5ccf636d676e9
singh-navdeep1 <singhnavdeep350@gmail.com>
2022-10-29 17:19:56 +0530

    sdadasd

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let popup = document.querySelector(".backdrop");
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closebutton(){
+    popup.classList.remove("open-popup")
+}
