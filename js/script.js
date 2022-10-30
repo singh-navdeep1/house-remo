d48668a056cf2b834a37181b6c1c91d326adc4c4
singh-navdeep1 <singhnavdeep350@gmail.com>
2022-10-30 16:18:45 +0530

    sss

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ function openPopup(){
 function closebutton(){
     popup.classList.remove("open-popup")
 }
+
+$('#toggle').click(function(e) {
+    e.stopPropagation();
+    $(".navlinks").toggleClass('mobile-menu')
+  });
+  $('body').click(function(e) {
+    if ($('.navlinks').hasClass('mobile-menu')) {
+      $(".navlinks").toggleClass('mobile-menu')
+    }
+  })
